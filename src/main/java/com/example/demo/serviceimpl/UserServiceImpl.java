489cae7ec0aa62456fa5570341ce4bc8a0f9d4c6
bala-amypo <bala@amypo.in>
2025-12-19 04:08:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.AuthService;
+
+@Service
+public class UserServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User register(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+        // password hashing skipped for Review-1
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+}
