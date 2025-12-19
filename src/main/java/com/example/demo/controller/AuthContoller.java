9597cdd5ad95e377e90f354120fc282641bc4a38
bala-amypo <bala@amypo.in>
2025-12-19 03:56:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthContoller.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    // constructor injection (as taught)
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // CREATE (Register user)
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        return ResponseEntity.ok(authService.register(user));
+    }
+
+    // READ (Get user by id)
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(authService.getById(id));
+    }
+}
