a6d05ba4839b1201333f943eea4cb8d21486c5fc
bala-amypo <bala@amypo.in>
2025-12-20 16:38:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -16,7 +16,6 @@ public class AuthController {
     private final UserService userService;
     private final JwtUtil jwtUtil;
 
-    // Constructor Injection
     public AuthController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
@@ -26,11 +25,9 @@ public class AuthController {
     @PostMapping("/register")
     public User register(@RequestBody Map<String, String> body) {
 
-        User user = User.builder()
-                .email(body.get("email"))
-                .password(body.get("password"))
-                .role(User.Role.STUDENT)
-                .build();
+        User user = new User();
+        user.setEmail(body.get("email"));
+        user.setPassword(body.get("password"));
 
         return userService.register(user);
     }
@@ -40,12 +37,10 @@ public class AuthController {
     public Map<String, String> login(@RequestBody Map<String, String> body) {
 
         User user = userService.findByEmail(body.get("email"));
-
         String token = jwtUtil.generateToken(user);
 
         Map<String, String> response = new HashMap<>();
         response.put("token", token);
-
         return response;
     }
 }
