e745365a7a0fe7a483634cad9c493bdbd857a40a
bala-amypo <bala@amypo.in>
2025-12-20 11:21:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Skill.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String skillName;
+
+    @Column(nullable = false)
+    private String category;
+
+    private String description;
+
+    private Double minCompetencyScore = 0.0;
+
+    private Boolean active = true;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getSkillName() { return skillName; }
+    public void setSkillName(String skillName) { this.skillName = skillName; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Double getMinCompetencyScore() { return minCompetencyScore; }
+    public void setMinCompetencyScore(Double minCompetencyScore) {
+        this.minCompetencyScore = minCompetencyScore;
+    }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
