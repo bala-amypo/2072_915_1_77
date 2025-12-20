3a404e6999215946b643b3617344987dc887fedb
bala-amypo <bala@amypo.in>
2025-12-20 14:50:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,35 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
 import com.example.demo.service.AuthService;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@Tag(name = "Auth")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    // Constructor injection
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
-    // POST /auth/register
     @PostMapping("/register")
-    public AuthResponse register(@RequestBody AuthRequest request) {
+    public ResponseEntity<String> register(@RequestBody AuthRequest request) {
         authService.register(request);
-        return new AuthResponse("User registered successfully");
-    }
-
-    // POST /auth/login
-    @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest request) {
-        return new AuthResponse("Login successful");
+        return ResponseEntity.ok("User registered successfully");
     }
 }
