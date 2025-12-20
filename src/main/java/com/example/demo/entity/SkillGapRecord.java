2a0d23a4219c9175aab3c25f8811bca4516140f7
bala-amypo <bala@amypo.in>
2025-12-20 15:17:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/SkillGapRecord.java
+++ src/main/java/com/example/demo/entity/SkillGapRecord.java
@@ -10,6 +10,12 @@ public class SkillGapRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    private StudentProfile studentProfile;
+
+    @ManyToOne
+    private Skill skill;
+
     private Double currentScore;
     private Double targetScore;
     private Double gapScore;
@@ -21,35 +27,5 @@ public class SkillGapRecord {
         this.calculatedAt = Instant.now();
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Double getCurrentScore() {
-        return currentScore;
-    }
-
-    public void setCurrentScore(Double currentScore) {
-        this.currentScore = currentScore;
-    }
-
-    public Double getTargetScore() {
-        return targetScore;
-    }
-
-    public void setTargetScore(Double targetScore) {
-        this.targetScore = targetScore;
-    }
-
-    public Double getGapScore() {
-        return gapScore;
-    }
-
-    public void setGapScore(Double gapScore) {
-        this.gapScore = gapScore;
-    }
-
-    public Instant getCalculatedAt() {
-        return calculatedAt;
-    }
+    // getters and setters
 }
