1de7c0d27b8585fef19100d5430d5e47671d646b
bala-amypo <bala@amypo.in>
2025-12-20 12:21:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/SkillGapResult.java
+++ src/main/java/com/example/demo/entity/SkillGapResult.java
@@ -12,7 +12,6 @@ public class SkillGapResult {
     @ManyToOne
     @JoinColumn(name = "student_profile_id", nullable = false)
     private StudentProfile studentProfile;
-    // Many skill gap results belong to one skill
     @ManyToOne
     @JoinColumn(name = "skill_id", nullable = false)
     private Skill skill;
@@ -22,13 +21,11 @@ public class SkillGapResult {
     private Double gapScore;
 
     private Instant calculatedAt;
-
     @PrePersist
     public void onCreate() {
         this.calculatedAt = Instant.now();
     }
 
-    // getters & setters
     public Long getId() {
         return id;
     }
@@ -36,7 +33,6 @@ public class SkillGapResult {
     public StudentProfile getStudentProfile() {
         return studentProfile;
     }
-
     public void setStudentProfile(StudentProfile studentProfile) {
         this.studentProfile = studentProfile;
     }
@@ -44,35 +40,27 @@ public class SkillGapResult {
     public Skill getSkill() {
         return skill;
     }
-
     public void setSkill(Skill skill) {
         this.skill = skill;
     }
-
     public Double getCurrentScore() {
         return currentScore;
     }
-
     public void setCurrentScore(Double currentScore) {
         this.currentScore = currentScore;
     }
-
     public Double getTargetScore() {
         return targetScore;
     }
-
     public void setTargetScore(Double targetScore) {
         this.targetScore = targetScore;
     }
-
     public Double getGapScore() {
         return gapScore;
     }
-
     public void setGapScore(Double gapScore) {
         this.gapScore = gapScore;
     }
-
     public Instant getCalculatedAt() {
         return calculatedAt;
     }
