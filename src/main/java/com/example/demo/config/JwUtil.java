b6ff89262648d910e617c982548ec12b72b6e8ca
bala-amypo <bala@amypo.in>
2025-12-23 08:40:39 +0000

    User commit

--- src/main/java/com/example/demo/config/JwUtil.java
+++ src/main/java/com/example/demo/config/JwUtil.java
@@ -1,5 +1,37 @@
 package com.example.demo.config;
 
-public class JwUtil {
-    
+import com.example.demo.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private final Key key;
+    private final long validityInMs;
+
+    public JwtUtil(String secret, long validityInMs) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.validityInMs = validityInMs;
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .claim("userId", user.getId())
+                .claim("email", user.getEmail())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + validityInMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Jws<Claims> validateAndParse(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
 }
