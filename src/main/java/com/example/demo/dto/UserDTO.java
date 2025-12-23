18028c023e22c5b5d8a4e73e993fcd6ab8bc0e56
bala-amypo <bala@amypo.in>
2025-12-23 12:45:11 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserDTO.java
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,51 +1,18 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.Instant;
 
+@Getter
+@AllArgsConstructor
 public class UserDTO {
+
     private Long id;
     private String fullName;
     private String email;
-    private String role;
+    private User.Role role;
     private Instant createdAt;
-
-    public UserDTO() {
-    }
-    public UserDTO(Long id, String fullName, String email, String role, Instant createdAt) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.role = role;
-        this.createdAt = createdAt;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getFullName() {
-        return fullName;
-    }
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
 }
